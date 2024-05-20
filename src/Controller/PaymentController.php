5743ebc43d49ff887d3d99a0af88f3bfc616b80f
Marwa <messaoudimarwa75@gmail.com>
2024-05-20 23:35:22 +0100

    myproject

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 use App\Entity\ReservationTour;
-use App\Entity\ReservationHotel;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,53 +19,39 @@ class PaymentController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $reservationId = $request->query->get('reservationId');
-        $reservationType = $request->query->get('type');
-
-        if ($reservationType === 'hotel') {
-            $reservation = $entityManager->getRepository(ReservationHotel::class)->find($reservationId);
-        } else if ($reservationType === 'tour') {
-            $reservation = $entityManager->getRepository(ReservationTour::class)->find($reservationId);
-        } else {
-            $this->addFlash('error', 'Invalid reservation type.');
-            return $this->redirectToRoute('home');
-        }
-
+        
+        $reservation = $entityManager->getRepository(ReservationTour::class)->find($reservationId);
+        
         if (!$reservation) {
             $this->addFlash('error', 'Reservation not found.');
             return $this->redirectToRoute('home');
         }
-
+        
         $payment = new Payement();
-        if ($reservationType === 'hotel') {
-            $payment->setPayementHotel($reservation);
-            $payment->setPrixTotal($reservation->getTotal());
-        } else {
-            $payment->setPeyementTour($reservation);
-            $payment->setPrixTotal($reservation->getTotal());
-        }
-
+        $payment->setprixTotal($reservation->getTotal()); // Set total price from reservation
+        
         $form = $this->createForm(PaymentType::class, $payment);
-
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
+            $payment->setPeyementTour($reservation);
             $entityManager->persist($payment);
             $entityManager->flush();
-
+        
+            $this->addFlash('success', 'Payement successful!');
             return $this->redirectToRoute('payment_success');
         }
-
+        
         return $this->render('payement/new.html.twig', [
             'form' => $form->createView(),
             'reservation' => $reservation,
         ]);
     }
-
-    /**
+        /**
      * @Route("/payment/success", name="payment_success")
      */
     public function success(): Response
     {
-        return $this->render('payment/success.html.twig');
+        return $this->render('payement/success.html.twig');
     }
-}
+    }
